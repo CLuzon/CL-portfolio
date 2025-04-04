71796b33cd6a6b2e13323ba701cd50a395cfa805
CLuzon <claur103luzon@gmail.com>
2025-04-04 21:36:40 +0900

    Changed the introduction page.
    Text on the introduction page that could not break lines under ideal conditions was corrected to allow line breaks.

--- JS/typeText.js
+++ JS/typeText.js
@@ -1,5 +1,7 @@
 const textEl = document.getElementById("introText");
+// idがintroTextの要素を取得
 let shinarioNum = 0;
+// shinarioNumを初期化
 document.addEventListener("keydown", function(event) {
 
     const shinarioTxt = [
@@ -10,17 +12,38 @@ document.addEventListener("keydown", function(event) {
         "ご意見がありましたら、ご気軽にお知らせください！",
         "また、私の制作物は「Report」からご覧いただけます",
         "文章はここまでです",
-        "再度初めからお読みになる際は二度エンターキーを押してください<br>それでは、良い一日を！",
+        "再度初めからお読みになる際は繰り返しエンターキーを押してください\nそれでは、良い一日を！",
         " "
     ];
-    textEl.innerHTML = shinarioTxt[shinarioNum];
+    // shinarioTxtの配列を作成
+
+
     // Enterキーが押されたときの処理
     if (event.key === "Enter") {
+        textEl.innerHTML = ""; // テキストを消去
+        let index = 0;
+
+
         console.log("Enter key pressed");
         shinarioNum = (shinarioNum + 1) % (shinarioTxt.length); 
         // shinarioNumを1増やす
         // shinarioNumがshinarioTxtの長さ以上になったらリセット;
         console.log(shinarioNum);
+
+        function type () {
+            if (index < shinarioTxt[shinarioNum].length) {
+                const char = shinarioTxt[shinarioNum].charAt(index);
+                // shinarioTxt[shinarioNum]のindex番目の文字を取得
+                if (char === "\n") {
+                    textEl.innerHTML += "<br>";
+                } else {
+                    textEl.innerHTML += char;
+                }
+                index++;
+                setTimeout(type, 50);
+            }
+        }
+        type();
     }
     
 });
